112b3ee9d329629453ec1eb7b3e0455bcf6b5b73
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-29 18:34:54 +0300

    Wrote failing test [Solver.factorial return 1 if input == 0]

--- solver.rb
+++ solver.rb
@@ -1,5 +1,6 @@
 class Solver
   def factorial(integer)
+
     res = 1
     for i in 1..integer
       res *= i
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,15 +6,22 @@ describe Solver do
   end
 
   describe '#factorial' do
-    it 'takes one argument an integer' do
+    it 'takes one argument' do
       expect{@solver.factorial()}.to raise_error(ArgumentError)
       expect{@solver.factorial(12)}.not_to raise_error
       expect{@solver.factorial(12, 12)}.to raise_error(ArgumentError)
     end
 
+    it 'takes only integer' do
+      # expect('3').to eql 6
+    end
 
     it 'returns factorial of input' do
       expect(@solver.factorial(3)).to eql 6
+    end
+
+    it 'return 1 if input is 0' do
+      expect(@solver.factorial(0)).to eql 1
     end
   end
 end
